1277bca362d448107ce1e08e6c55a9dabce95bac
marcomoreno <marko.moreno12@gmail.com>
2020-11-10 21:36:47 -0600

    First commit!

---
+++ app/src/main/java/com/test/wear/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.test.wear
+
+import android.os.Bundle
+import android.support.wearable.activity.WearableActivity
+
+class MainActivity : WearableActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setAmbientEnabled()
+    }
+}
